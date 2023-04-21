9d57c3a8d440cd61ec2f706e2832e6c7c258d0b9
Hanane OUBELKAS <houbelkas@laayoune.local>
2023-04-21 13:23:27 +0000

    commi

--- src/main/java/com/example/demo/dao/AnnexeRepository.java
+++ src/main/java/com/example/demo/dao/AnnexeRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.demo.entities.Annexe;
 
+@CrossOrigin(origins = "*")
 public interface AnnexeRepository extends JpaRepository<Annexe, Integer> {
 
 }
--- src/main/java/com/example/demo/dao/BenificiaireRepository.java
+++ src/main/java/com/example/demo/dao/BenificiaireRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.demo.entities.Benificiaire;
 import com.example.demo.entities.Categorie;
 
+@CrossOrigin(origins = "*")
 public interface BenificiaireRepository extends JpaRepository<Benificiaire, Integer> {
 
 }
